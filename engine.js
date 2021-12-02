1c046659efaef1e2e816186c289c89290026f586
Flavio Carrara De Capua <flavio@carrara.us>
2021-12-02 10:33:12 -0300

    1000/60

--- engine.js
+++ engine.js
@@ -79,7 +79,7 @@ function log(t){
 	logger.innerHTML=t;
 }
 
-window.requestAnimFrame = (function(){ return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 1000 / 60); }; })(); // usage: // instead of setInterval(render, 16) .... 
+window.requestAnimFrame = (function(){ return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ){ window.setTimeout(callback, 10000 / 60); }; })(); // usage: // instead of setInterval(render, 16) .... 
  (function animloop(){ requestAnimFrame(animloop); if (
  			(ignited) && (!onhold)
  			){render();} })();
